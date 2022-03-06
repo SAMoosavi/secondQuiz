fb57b2a063f1b1a5f684a1dcdb0c7b8a591d67c6
Moosavi <moosavi238@gmail.com>
2022-03-06 12:02:21 +0330

    creat and edit input

--- resources/js/component/Input.vue
+++ resources/js/component/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <input
-        class="border-1 focus:border input input-bordered outline-0 focus:outline-0 focus:border-primary-focus focus:ring-2 focus:ring-primary focus:ring-opacity-50 w-full"
+        class="border-1 focus:border border-primary input input-bordered outline-0 focus:outline-0 focus:border-primary-focus focus:ring-2 focus:ring-primary focus:ring-opacity-50 w-full"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"

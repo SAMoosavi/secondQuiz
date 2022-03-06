49b2fd0a16aac248e13ae18820b5358778476026
Moosavi <moosavi238@gmail.com>
2022-03-06 12:03:32 +0330

    creat auth link

---
+++ resources/js/component/AuthLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <Link
+        :method="method"
+        :href="href"
+        class="link opacity-50 hover:opacity-100 underline-offset-4 decoration-dashed hover:decoration-solid focus:decoration-solid transition-all duration-300"
+    >
+        <slot />
+    </Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    href: String,
+    method: {
+        method: String,
+        default: "get",
+    },
+});
+</script>
+
+<style></style>

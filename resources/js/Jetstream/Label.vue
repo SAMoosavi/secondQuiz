a6f7d5b8f45d795452087e3b2b085ab4b3ffde7d
Moosavi <moosavi238@gmail.com>
2022-03-06 12:02:07 +0330

    creat and edit label

--- resources/js/Jetstream/Label.vue
+++ resources/js/Jetstream/Label.vue
@@ -1,14 +1,15 @@
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label
+        class="block text-sm font-medium after:text-error after:mr-0.5 after:text-opacity-0 after:content-['*']"
+        :class="{
+            'after:text-opacity-100': required,
+        }"
+    >
         <span v-if="value">{{ value }}</span>
         <span v-else><slot></slot></span>
     </label>
 </template>
 
-<script>
-    import { defineComponent } from 'vue'
-
-    export default defineComponent({
-        props: ['value']
-    })
+<script setup>
+defineProps(["value", "required"]);
 </script>
---
+++ resources/js/component/Label.vue
@@ -0,0 +1,15 @@
+<template>
+    <label
+        class="text-sm font-medium justify-start label cursor-pointer after:text-error after:mr-0.5 after:text-opacity-0 after:content-['*']"
+        :class="{
+            'after:text-opacity-100': required,
+        }"
+    >
+        <template v-if="value">{{ value }}</template>
+        <template v-else><slot></slot></template>
+    </label>
+</template>
+
+<script setup>
+defineProps(["value", "required"]);
+</script>

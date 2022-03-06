d5d8ad6a44faf021d30ada4afeb4cf5f9e2fca1f
Moosavi <moosavi238@gmail.com>
2022-03-06 12:03:42 +0330

    edit login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,123 +1,119 @@
 <template>
-    <Head title="Log in" />
+    <Head title="ورود" />
 
-    <jet-authentication-card>
+    <my-authentication-card>
         <template #logo>
             <jet-authentication-card-logo />
         </template>
+        <template #title>ورود</template>
 
-        <jet-validation-errors class="mb-4" />
-
-        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="email" value="Email" />
-                <jet-input
+        <form @submit.prevent="submit" novalidate>
+            <div class="form-control">
+                <my-label for="email" value="ایمیل" :required="true" />
+                <my-input
                     id="email"
                     type="email"
-                    class="block w-full mt-1"
                     v-model="form.email"
                     required
                     autofocus
+                    autocomplete="email"
                 />
             </div>
 
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input
+            <div class="mt-4 form-control">
+                <my-label for="password" value="رمز ورود" :required="true" />
+                <my-input
                     id="password"
                     type="password"
-                    class="block w-full mt-1"
                     v-model="form.password"
                     required
                     autocomplete="current-password"
                 />
             </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <jet-checkbox
-                        name="remember"
-                        v-model:checked="form.remember"
-                    />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
+            <div class="block mt-4 form-control">
+                <my-checkbox
+                    txt="من را به خاطر بسپار"
+                    v-model="form.remember"
+                />
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="text-sm text-gray-600 underline hover:text-gray-900"
-                >
-                    Forgot your password?
-                </Link>
-
-                <jet-button
-                    class="ml-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Log in
-                </jet-button>
+            <div class="card-actions flex-col items-start justify-end">
+                <div class="flex items-center justify-end mt-4 mr-auto">
+                    <my-auth-link :href="route('register')">
+                        ساخت حساب جدید
+                    </my-auth-link>
+                    <my-button
+                        class="mr-4"
+                        :loding="loding"
+                        :disabled="form.processing"
+                    >
+                        ورود
+                    </my-button>
+                </div>
+                <div class="form-control">
+                    <my-auth-link :href="route('password.request')">
+                        فراموشی رمز عبور!
+                    </my-auth-link>
+                </div>
             </div>
         </form>
-    </jet-authentication-card>
+    </my-authentication-card>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import JetAuthenticationCard from "@/Jetstream/AuthenticationCard.vue";
+<script setup>
+import MyAuthenticationCard from "@/component/AuthenticationCard.vue";
+import MyAuthLink from "@/component/AuthLink.vue";
+import MyLabel from "@/component/Label.vue";
+import MyCheckbox from "@/component/Checkbox.vue";
+import MyInput from "@/component/Input.vue";
+import MyButton from "@/component/Button.vue";
+
 import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo.vue";
-import JetButton from "@/Jetstream/Button.vue";
-import JetInput from "@/Jetstream/Input.vue";
-import JetCheckbox from "@/Jetstream/Checkbox.vue";
-import JetLabel from "@/Jetstream/Label.vue";
 import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
-import { Head, Link } from "@inertiajs/inertia-vue3";
 
-export default defineComponent({
-    components: {
-        Head,
-        JetAuthenticationCard,
-        JetAuthenticationCardLogo,
-        JetButton,
-        JetInput,
-        JetCheckbox,
-        JetLabel,
-        JetValidationErrors,
-        Link,
-    },
+import { ref } from "vue";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
 
-    props: {
-        canResetPassword: Boolean,
-        status: String,
-    },
-
-    data() {
-        return {
-            form: this.$inertia.form({
-                email: "",
-                password: "",
-                remember: false,
-            }),
-        };
-    },
-
-    methods: {
-        submit() {
-            this.form
-                .transform((data) => ({
-                    ...data,
-                    remember: this.form.remember ? "on" : "",
-                }))
-                .post(this.route("login"), {
-                    onFinish: () => this.form.reset("password"),
-                });
-        },
-    },
+const props = defineProps({
+    canResetPassword: Boolean,
+    status: String,
 });
+
+const form = useForm({
+    email: "",
+    password: "",
+    remember: false,
+});
+
+const loding = ref(false);
+
+function validEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
+function submit() {
+    let valid = true;
+    loding.value = true;
+    if (!form.email || !form.password) {
+        valid = false;
+    }
+    if (form.email && !validEmail(form.email)) {
+        valid = false;
+    }
+    if (valid) {
+        form.transform((data) => ({
+            ...data,
+            remember: form.remember ? "on" : "",
+        })).post(route("login"), {
+            onFinish: () => form.reset("password"),
+        });
+    }
+    setTimeout(() => {
+        loding.value = false;
+    }, 200);
+}
 </script>

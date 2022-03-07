95a3ca459a970cdd8ead01838fbbf2847427b9fe
Moosavi <moosavi238@gmail.com>
2022-03-07 11:43:26 +0330

    edit auth...card

--- resources/js/component/AuthenticationCard.vue
+++ resources/js/component/AuthenticationCard.vue
@@ -1,6 +1,7 @@
 <template>
+    <bg-auth />
     <div
-        class="min-h-screen flex flex-col justify-center items-center bg-base-100"
+        class="min-h-screen flex flex-col justify-center items-center bg-transparent"
     >
         <div>
             <slot name="logo" />
@@ -8,10 +9,17 @@
 
         <div class="w-full sm:max-w-md card bg-base-300 m-4">
             <div class="card-body">
-                <h2 class="card-title text-center text-primary-focus self-center"><slot name="title" /></h2>
+                <h2
+                    class="card-title text-center text-primary-focus self-center"
+                >
+                    <slot name="title" />
+                </h2>
 
                 <slot />
             </div>
         </div>
     </div>
 </template>
+<script setup>
+import BgAuth from "@/component/BgAuth.vue";
+</script>

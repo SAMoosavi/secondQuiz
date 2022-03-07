e9f68e3ce5b951e22f3c1c8bf8775faa92125661
Moosavi <moosavi238@gmail.com>
2022-03-07 11:43:36 +0330

    ...

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,5 +3,3 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// import anime from 'animejs/lib/anime.es.js';

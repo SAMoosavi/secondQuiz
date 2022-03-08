ff41a3d54b78c05b5a77ec2ee5736e57025521c8
Moosavi <moosavi238@gmail.com>
2022-03-08 14:59:16 +0330

    okey comfirm pass

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -1,67 +1,60 @@
 <template>
-    <Head title="Secure Area" />
-
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
-        </template>
-
-        <div class="mb-4 text-sm text-gray-600">
-            This is a secure area of the application. Please confirm your password before continuing.
+    <Head title="منطقه امن" />
+    <my-authentication-card>
+        <template #title>منطقه امن</template>
+        <div class="mb-4 text-sm text-justify text-primary-content">
+            این یک منطقه امن برنامه است. لطفاً قبل از ادامه رمز عبور خود را
+            تأیید کنید.
         </div>
 
-        <jet-validation-errors class="mb-4" />
-
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" autofocus />
+        <form @submit.prevent="submit" novalidate>
+            <div class="form-control">
+                <my-label for="password" value="رمز عبور" :required="true" />
+                <my-input
+                    id="password"
+                    type="password"
+                    v-model="form.password"
+                    required
+                    autocomplete="current-password"
+                    autofocus
+                />
             </div>
 
-            <div class="flex justify-end mt-4">
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Confirm
-                </jet-button>
+            <div class="items-end justify-end mt-4 mr-auto form-control">
+                <my-button :loding="loding" :disabled="form.processing">
+                    تایید
+                </my-button>
             </div>
         </form>
-    </jet-authentication-card>
+    </my-authentication-card>
 </template>
 
-<script>
-    import { defineComponent } from 'vue';
-    import { Head } from '@inertiajs/inertia-vue3';
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
+<script setup>
+import MyAuthenticationCard from "@/component/AuthenticationCard.vue";
+import MyButton from "@/component/Button.vue";
+import MyLabel from "@/component/Label.vue";
+import MyInput from "@/component/Input.vue";
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetLabel,
-            JetValidationErrors
-        },
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+import { ref } from "@vue/reactivity";
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    password: '',
-                })
-            }
-        },
+const form = useForm({
+    password: "",
+});
 
-        methods: {
-            submit() {
-                this.form.post(this.route('password.confirm'), {
-                    onFinish: () => this.form.reset(),
-                })
-            }
-        }
-    })
+const loding = ref(false);
+
+function submit() {
+    loding.value = true;
+    if (!form.password) {
+        //
+    } else {
+        form.post(route("password.confirml"), {
+            onFinish: () => form.reset(),
+        });
+    }
+    setTimeout(() => {
+        loding.value = false;
+    }, 200);
+}
 </script>

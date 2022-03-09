c599a75e8deb1523d7eff34271241f6f4e76550c
Moosavi <moosavi238@gmail.com>
2022-03-09 12:39:39 +0330

    edit for them

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,7 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" data-theme="light">
         @inertia
 
         @env ('local')

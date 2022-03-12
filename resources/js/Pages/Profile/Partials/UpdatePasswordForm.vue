0e50e4286e069ff3cff360613114c29243d2b9d3
Moosavi <moosavi238@gmail.com>
2022-03-12 22:44:15 +0330

    okeyed update password

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -1,93 +1,112 @@
 <template>
-    <jet-form-section @submitted="updatePassword">
-        <template #title>
-            Update Password
-        </template>
-
-        <template #description>
-            Ensure your account is using a long, random password to stay secure.
-        </template>
-
-        <template #form>
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="current_password" value="Current Password" />
-                <jet-input id="current_password" type="password" class="mt-1 block w-full" v-model="form.current_password" ref="current_password" autocomplete="current-password" />
-                <jet-input-error :message="form.errors.current_password" class="mt-2" />
+    <div class="flex justify-center w-full">
+        <form @submit.prevent="send" class="w-1/2" novalidate>
+            <div class="form-control">
+                <my-label
+                    for="current_password"
+                    value="رمز عبور فعلی"
+                    :required="true"
+                />
+                <my-input
+                    id="current_password"
+                    type="password"
+                    v-model="form.current_password"
+                    ref="current_password"
+                    autocomplete="current-password"
+                />
             </div>
 
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password" value="New Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" ref="password" autocomplete="new-password" />
-                <jet-input-error :message="form.errors.password" class="mt-2" />
+            <div class="form-control">
+                <my-label
+                    for="password"
+                    value="رمز عبور جدید"
+                    :required="true"
+                />
+                <my-input
+                    id="password"
+                    type="password"
+                    v-model="form.password"
+                    ref="password"
+                    autocomplete="new-password"
+                />
             </div>
 
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" autocomplete="new-password" />
-                <jet-input-error :message="form.errors.password_confirmation" class="mt-2" />
+            <div class="form-control">
+                <my-label
+                    for="password_confirmation"
+                    value="تکرار رمز عبور جدید"
+                    :required="true"
+                />
+                <my-input
+                    id="password_confirmation"
+                    type="password"
+                    v-model="form.password_confirmation"
+                    autocomplete="new-password"
+                />
             </div>
-        </template>
-
-        <template #actions>
-            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Saved.
-            </jet-action-message>
-
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Save
-            </jet-button>
-        </template>
-    </jet-form-section>
+            <div class="mt-2 form-control">
+                <my-button :loding="loding" :disabled="form.processing">
+                    ذخیره
+                </my-button>
+            </div>
+        </form>
+    </div>
 </template>
 
-<script>
-    import { defineComponent } from 'vue'
-    import JetActionMessage from '@/Jetstream/ActionMessage.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetFormSection from '@/Jetstream/FormSection.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetInputError from '@/Jetstream/InputError.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
+<script setup>
+import myButton from "@/component/Form/Button.vue";
+import myInput from "@/component/Form/Input.vue";
+import myLabel from "@/component/Form/Label.vue";
 
-    export default defineComponent({
-        components: {
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-        },
+import {
+    passwordConfirmation,
+    password as validationsPassword,
+} from "@/functions/validations";
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    current_password: '',
-                    password: '',
-                    password_confirmation: '',
-                }),
-            }
-        },
+import { useForm } from "@inertiajs/inertia-vue3";
+import { ref } from "@vue/reactivity";
 
-        methods: {
-            updatePassword() {
-                this.form.put(route('user-password.update'), {
-                    errorBag: 'updatePassword',
-                    preserveScroll: true,
-                    onSuccess: () => this.form.reset(),
-                    onError: () => {
-                        if (this.form.errors.password) {
-                            this.form.reset('password', 'password_confirmation')
-                            this.$refs.password.focus()
-                        }
+const form = useForm({
+    current_password: "",
+    password: "",
+    password_confirmation: "",
+});
 
-                        if (this.form.errors.current_password) {
-                            this.form.reset('current_password')
-                            this.$refs.current_password.focus()
-                        }
-                    }
-                })
+const password = ref();
+const current_password = ref();
+const loding = ref(false);
+function send() {
+    loding.value = true;
+    if (
+        !form.current_password ||
+        !form.password ||
+        !form.password_confirmation
+    ) {
+        //
+    } else if (!validationsPassword(form.password)) {
+        //password(form.password) return errors
+    } else if (
+        !passwordConfirmation(form.password, form.password_confirmation)
+    ) {
+        //
+    } else {
+        form.put(route("user-password.update"), {
+            onSuccess: () => form.reset(),
+            onError: () => {
+                if (form.errors.password) {
+                    form.reset("password", "password_confirmation");
+                    password.value.focus();
+                }
+
+                if (form.errors.current_password) {
+                    form.reset("current_password");
+                    current_password.value.focus();
+                }
             },
-        },
-    })
+        });
+    }
+    setTimeout(() => {
+        loding.value = false;
+    }, 200);
+}
 </script>

35317a6c03f60de1a0d5912f1885505a02bb177b
Moosavi <moosavi238@gmail.com>
2022-03-12 11:13:17 +0330

    creat select component

---
+++ resources/js/component/Form/Select.vue
@@ -0,0 +1,12 @@
+<template>
+    <select
+        class="select select-primary bg-transparent focus:select-primary text-primary ring-0 outline-0 focus:ring-0 focus:outline-0"
+    >
+        <slot />
+    </select>
+</template>
+
+<script>
+
+
+</script>

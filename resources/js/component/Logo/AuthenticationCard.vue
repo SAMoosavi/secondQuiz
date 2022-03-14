6d66f3d16c7aacf1570986ff16aa87ba1ec4dca8
Moosavi <moosavi238@gmail.com>
2022-03-14 10:18:12 +0330

    edite logo link to component

--- resources/js/component/Logo/AuthenticationCard.vue
+++ resources/js/component/Logo/AuthenticationCard.vue
@@ -1,10 +1,8 @@
 <template>
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
+    <application-mark
         class="w-20 p-4 rounded-t-full stroke-current stroke-2 bg-primary/75 text-primary-content"
-        fill="none"
-        viewBox="0 0 40 30"
-    >
-        <path d="M0 15L20 0L40 15L20 30z M20 0L20 30 M0 15L40 15 M10 15L20 7.5L30 15L20 22.5z M10 7.5L30 22.5 M10 22.5 L30 7.5" />
-    </svg>
+    />
 </template>
+<script setup>
+import ApplicationMark from "./ApplicationMark.vue";
+</script>

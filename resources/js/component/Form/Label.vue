f2630eb9de9f9754afb3c04a1002688eab1dd293
Moosavi <moosavi238@gmail.com>
2022-03-14 10:09:01 +0330

    edite txt color

--- resources/js/component/Form/Label.vue
+++ resources/js/component/Form/Label.vue
@@ -1,6 +1,6 @@
 <template>
     <label
-        class="text-sm font-medium justify-start label cursor-pointer after:text-error after:mr-0.5 after:text-opacity-0 after:content-['*']"
+        class="text-sm font-medium justify-start text-base-content label cursor-pointer after:text-error after:mr-0.5 after:text-opacity-0 after:content-['*']"
         :class="{
             'after:text-opacity-100': required,
         }"

6d151c4a12950bd78b68bc9a9201a775193a3372
Moosavi <moosavi238@gmail.com>
2022-03-14 09:46:02 +0330

    create nav link

---
+++ resources/js/component/Navbar/Link.vue
@@ -0,0 +1,22 @@
+<template>
+    <Link
+        :href="href"
+        class="hidden btn sm:inline-flex"
+        :class="{ 'btn-primary ': active }"
+    >
+        <slot />
+    </Link>
+    <Link
+        :href="href"
+        class="block p-2 sm:hidden"
+        :class="{ 'bg-primary-focus border-l-8 border-primary ': active }"
+    >
+        <slot />
+    </Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps(["href", "active"]);
+</script>

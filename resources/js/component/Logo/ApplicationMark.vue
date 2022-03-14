fa0498492a2fd5a82360adc9e50670f944cda5c7
Moosavi <moosavi238@gmail.com>
2022-03-14 09:46:17 +0330

    creat logo from app

---
+++ resources/js/component/Logo/ApplicationMark.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-12 stroke-current stroke-2 "
+        fill="none"
+        viewBox="0 0 40 30"
+    >
+        <path d="M0 15L20 0L40 15L20 30z M20 0L20 30 M0 15L40 15 M10 15L20 7.5L30 15L20 22.5z M10 7.5L30 22.5 M10 22.5 L30 7.5" />
+    </svg>
+</template>

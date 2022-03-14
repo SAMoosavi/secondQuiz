eba52c9f905681870970d560a3c36b1066054481
Moosavi <moosavi238@gmail.com>
2022-03-14 10:26:24 +0330

    creat select

--- resources/js/component/Form/Select.vue
+++ resources/js/component/Form/Select.vue
@@ -1,12 +1,16 @@
 <template>
     <select
-        class="select select-primary bg-transparent focus:select-primary text-primary ring-0 outline-0 focus:ring-0 focus:outline-0"
+        :name="name"
+        :id="id"
+        @input="$emit('update:modelValue', $event.target.value)"
+        :required="required"
+        class="bg-transparent select select-primary focus:select-primary text-primary ring-0 outline-0 focus:ring-0 focus:outline-0"
     >
         <slot />
     </select>
 </template>
 
-<script>
-
-
+<script setup>
+defineProps(["name", "id", "required"]);
+defineEmits(["update:modelValue"]);
 </script>

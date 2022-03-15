4f37a50e88bc2de8a27f4900bc21bf9905adfb65
Moosavi <moosavi238@gmail.com>
2022-03-16 01:26:43 +0330

    creat cheng icon for dashbord component

---
+++ resources/js/component/Dashboard/ChengIcone.vue
@@ -0,0 +1,14 @@
+<template>
+    <transition
+        enter-active-class="transition duration-100 origin-center"
+        enter-from-class="transform -rotate-90"
+        enter-to-class="transform rotate-0"
+        leave-active-class="transition duration-100 origin-center"
+        leave-from-class="transform rotate-0"
+        leave-to-class="transform rotate-90"
+        mode="out-in"
+    >
+        <slot />
+    </transition>
+</template>
+

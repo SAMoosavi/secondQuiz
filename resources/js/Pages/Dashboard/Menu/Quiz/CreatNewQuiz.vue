864210eec0d405572df8197c8460b20162275b26
Moosavi <moosavi238@gmail.com>
2022-03-16 01:27:59 +0330

    creat create new quiz

---
+++ resources/js/Pages/Dashboard/Menu/Quiz/CreatNewQuiz.vue
@@ -0,0 +1,20 @@
+<template>
+    <div @click="$emit('showThis', -2)">
+        <p
+            class="flex justify-between items-center px-4 py-2"
+            :class="{ 'bg-primary': show }"
+        >
+            ساخت آزمون جدید
+            <span>
+                <plus />
+            </span>
+        </p>
+    </div>
+</template>
+
+<script setup>
+import Plus from "@/component/Icons/Plus.vue";
+
+defineProps(["show"]);
+defineEmits(["showThis"]);
+</script>

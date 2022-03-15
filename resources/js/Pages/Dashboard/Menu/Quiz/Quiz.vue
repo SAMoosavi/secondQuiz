3b541da2155f56a32620fda49a7b76ae5158c77a
Moosavi <moosavi238@gmail.com>
2022-03-16 01:27:43 +0330

    creat Quiz in Quiz menu

---
+++ resources/js/Pages/Dashboard/Menu/Quiz/Quiz.vue
@@ -0,0 +1,28 @@
+<template>
+    <p
+        class="flex justify-between items-center px-4 py-2"
+        :class="{ 'bg-primary': show }"
+        @click="$emit('showThis', show ? -1 : index)"
+    >
+        {{ quiz.name }}
+        <span>
+            <cheng-icone>
+                <chevron-up v-if="show" />
+                <chevron-down v-else />
+            </cheng-icone>
+        </span>
+    </p>
+    <div v-if="show" class="h-12 bg-primary-focus"></div>
+</template>
+
+<script setup>
+import ChengIcone from "@/component/Dashboard/ChengIcone.vue";
+
+import ChevronUp from "@/component/Icons/ChevronUp.vue";
+import ChevronDown from "@/component/Icons/ChevronDown.vue";
+
+import { ref } from "@vue/reactivity";
+
+defineProps(["quiz", "show", "index"]);
+defineEmits(["showThis"]);
+</script>

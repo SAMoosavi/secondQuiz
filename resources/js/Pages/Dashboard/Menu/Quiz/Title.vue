a62252990fbb1f6f5f5e199b0c0ab070744d02ac
Moosavi <moosavi238@gmail.com>
2022-03-16 01:27:29 +0330

    creat title in Quiz menu

---
+++ resources/js/Pages/Dashboard/Menu/Quiz/Title.vue
@@ -0,0 +1,19 @@
+<template>
+    <my-title @cheng="chengShowQuiz" :show="showQuiz">آزمون ها ساخته شده</my-title>
+</template>
+
+<script setup>
+import MyTitle from '@/component/Dashboard/Title.vue';
+import { ref } from "@vue/reactivity";
+
+const showQuiz = ref(true);
+
+const emits = defineEmits(["sendShowQuiz"]);
+
+function chengShowQuiz() {
+    emits("sendShowQuiz", showQuiz.value);
+    showQuiz.value = !showQuiz.value;
+}
+</script>
+
+<style></style>

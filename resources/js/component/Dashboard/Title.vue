34939703128ea77910a1fd7e9b8fca74df8c5c6d
Moosavi <moosavi238@gmail.com>
2022-03-16 01:27:06 +0330

    creat title in dashbord component

---
+++ resources/js/component/Dashboard/Title.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="sticky top-0 bg-base-100">
+        <p
+            class="flex justify-between items-center px-4 py-2"
+            @click="$emit('cheng', show)"
+        >
+            <slot />
+            <span>
+                <cheng-icone>
+                    <chevron-up v-if="show" />
+                    <chevron-down v-else />
+                </cheng-icone>
+            </span>
+        </p>
+    </div>
+</template>
+
+<script setup>
+import ChevronUp from "@/component/Icons/ChevronUp.vue";
+import ChevronDown from "@/component/Icons/ChevronDown.vue";
+
+import ChengIcone from "@/component/Dashboard/ChengIcone.vue";
+
+defineProps(["show"]);
+defineEmits(["cheng"]);
+</script>
+
+<style></style>

29c7c6dd4ae8b06ff7ee93a617eb8b7890292b0c
Moosavi <moosavi238@gmail.com>
2022-03-16 17:34:19 +0330

    creat title for answer

---
+++ resources/js/Pages/Dashboard/Menu/Answer/Title.vue
@@ -0,0 +1,12 @@
+<template>
+    <my-title @cheng="$emit('sendShowAnswer')" :show="show">
+        آزمون هایی که شرکت کردید
+    </my-title>
+</template>
+
+<script setup>
+import MyTitle from "@/component/Dashboard/Title.vue";
+
+defineEmits(["sendShowAnswer"]);
+defineProps(["show"]);
+</script>

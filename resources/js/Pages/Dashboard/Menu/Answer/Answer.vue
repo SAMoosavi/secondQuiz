2567b552729f6ec196b52a5a2e22a0ec8324e4cf
Moosavi <moosavi238@gmail.com>
2022-03-16 17:34:31 +0330

    creat answer

---
+++ resources/js/Pages/Dashboard/Menu/Answer/Answer.vue
@@ -0,0 +1,27 @@
+<template>
+    <p
+        class="flex items-center justify-between px-4 py-2 transition-all duration-500"
+        :class="{ 'bg-primary': show }"
+        @click="$emit('showThis', show ? -2 : index)"
+    >
+        {{ answer.name }}
+        <span>
+            <cheng-icone>
+                <chevron-left v-if="show" />
+                <chevron-right v-else />
+            </cheng-icone>
+        </span>
+    </p>
+</template>
+
+<script setup>
+import ChengIcone from "@/component/Dashboard/ChengIcone.vue";
+
+// import ChevronLeft from "@/component/Icons/ChevronLeft.vue";
+// import ChevronRight from "@/component/Icons/ChevronRight.vue";
+
+import { ref } from "@vue/reactivity";
+
+defineProps(["answer", "show", "index"]);
+defineEmits(["showThis"]);
+</script>

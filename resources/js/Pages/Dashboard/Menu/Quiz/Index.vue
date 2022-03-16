ee8ac7dfbf58f1371bb2a7657aef684a89164ca5
Moosavi <moosavi238@gmail.com>
2022-03-16 17:32:43 +0330

    edite index page

--- resources/js/Pages/Dashboard/Menu/Quiz/Index.vue
+++ resources/js/Pages/Dashboard/Menu/Quiz/Index.vue
@@ -1,16 +1,25 @@
 <template>
-    <my-title @sendShowQuiz="sendShowQuiz" />
-    <div ref="quizDiv" class="overflow-hidden">
-        <creat-new-quiz :show="-2 == showIndex" @showThis="chengShowIndex" />
-        <div v-for="(quiz, index) in quizs" :key="quiz.name">
-            <quiz
-                :quiz="quiz"
-                :index="index"
-                :show="index == showIndex"
-                @showThis="chengShowIndex"
-            />
-        </div>
-    </div>
+    <my-title @sendShowQuiz="$emit('chengShow')" :show="show" />
+        <transition
+            @before-enter="onBeforEnter"
+            @enter="onEnter"
+            @leave="onLeave"
+        >
+            <div v-if="show" class="overflow-hidden">
+                <creat-new-quiz
+                    :show="-1 == showIndex"
+                    @showThis="chengShowIndex"
+                />
+                <div v-for="(quiz, index) in quizs" :key="quiz.name">
+                    <quiz
+                        :quiz="quiz"
+                        :index="index"
+                        :show="index == showIndex"
+                        @showThis="chengShowIndex"
+                    />
+                </div>
+            </div>
+        </transition>
 </template>
 
 <script setup>
@@ -18,44 +27,25 @@ import MyTitle from "@/Pages/Dashboard/Menu/Quiz/Title.vue";
 import Quiz from "@/Pages/Dashboard/Menu/Quiz/Quiz.vue";
 import CreatNewQuiz from "@/Pages/Dashboard/Menu/Quiz/CreatNewQuiz.vue";
 
-import anime from "animejs/lib/anime.es.js";
+import { open, close } from "@/functions/Anime";
 
 import { reactive, ref } from "@vue/reactivity";
+import { watch } from "@vue/runtime-core";
 
-let quizDivHeight = 0;
-const showQuiz = ref(true);
-const quizDiv = ref();
+const props = defineProps(["show"]);
+const emits = defineEmits(["chengShow"]);
 
 const showIndex = ref(-1);
 function chengShowIndex(index) {
     showIndex.value = index;
 }
-function sendShowQuiz() {
-    quizDivHeight = quizDiv.value.clientHeight
-        ? quizDiv.value.clientHeight
-        : quizDivHeight;
 
-    showQuiz.value = !showQuiz.value;
-    if (!showQuiz.value) {
-        close(quizDiv.value);
-    } else {
-        open(quizDiv.value, quizDivHeight);
-    }
+function onEnter(el, done) {
+    open(el, done);
 }
-function close(el) {
-    anime({
-        targets: el,
-        height: 0,
-        duration: 500,
-    });
-}
-function open(el, quizDivHeight) {
-    anime({
-        targets: el,
-        height: quizDivHeight,
-        ease: "linner",
-        duration: 500,
-    });
+
+function onLeave(el, done) {
+    close(el, done);
 }
 
 const quizs = reactive([

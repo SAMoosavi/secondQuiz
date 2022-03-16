b60b77d49fb2a7b31bc6d4f42346797c14c6b488
Moosavi <moosavi238@gmail.com>
2022-03-16 17:33:03 +0330

    edite title of Quiz

--- resources/js/Pages/Dashboard/Menu/Quiz/Title.vue
+++ resources/js/Pages/Dashboard/Menu/Quiz/Title.vue
@@ -1,19 +1,12 @@
 <template>
-    <my-title @cheng="chengShowQuiz" :show="showQuiz">آزمون ها ساخته شده</my-title>
+    <my-title @cheng="$emit('sendShowQuiz')" :show="show">
+        آزمون ها ساخته شده
+    </my-title>
 </template>
 
 <script setup>
-import MyTitle from '@/component/Dashboard/Title.vue';
-import { ref } from "@vue/reactivity";
+import MyTitle from "@/component/Dashboard/Title.vue";
 
-const showQuiz = ref(true);
-
-const emits = defineEmits(["sendShowQuiz"]);
-
-function chengShowQuiz() {
-    emits("sendShowQuiz", showQuiz.value);
-    showQuiz.value = !showQuiz.value;
-}
+defineEmits(["sendShowQuiz"]);
+defineProps(["show"]);
 </script>
-
-<style></style>

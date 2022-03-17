60c9e3a3593ed5747cc3c8064aa50fa68329dd15
Moosavi <moosavi238@gmail.com>
2022-03-17 11:28:38 +0330

    creat sub title component

---
+++ resources/js/component/Dashboard/SubTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <p
+        class="flex items-center justify-between px-4 py-2 transition-all duration-500"
+        :class="{ 'bg-primary': show }"
+    >
+        <slot />
+        <span>
+            <cheng-icone>
+                <template v-if="show">
+                    <slot name="iconShow" />
+                </template>
+                <template v-else>
+                    <slot name="iconNotShow" />
+                </template>
+            </cheng-icone>
+            <slot name="icon" />
+        </span>
+    </p>
+</template>
+
+<script setup>
+import ChengIcone from "@/component/Dashboard/ChengIcone.vue";
+
+defineProps(["show"]);
+</script>
+
+<style></style>

4e8dbe3af238fa0833d04a25a1112c1d3160ff0a
Moosavi <moosavi238@gmail.com>
2022-03-17 11:29:20 +0330

    edit quiz to componentebal

--- resources/js/Pages/Dashboard/Menu/Quiz/Quiz.vue
+++ resources/js/Pages/Dashboard/Menu/Quiz/Quiz.vue
@@ -1,33 +1,29 @@
 <template>
-    <p
-        class="flex items-center justify-between px-4 py-2 transition-all duration-500"
-        :class="{ 'bg-primary': show }"
-        @click="$emit('showThis', show ? -2 : index)"
-    >
-        {{ quiz.name }}
-        <span>
-            <cheng-icone>
-                <chevron-up v-if="show" />
-                <chevron-down v-else />
-            </cheng-icone>
-        </span>
-    </p>
+    <div @click="$emit('showThis', show ? -2 : index)">
+        <sub-title :show="show">
+            {{ quiz.name }}
+            <template #iconShow>
+                <chevron-up />
+            </template>
+            <template #iconNotShow>
+                <chevron-down />
+            </template>
+        </sub-title>
+    </div>
     <transition @enter="onEnter" @leave="onLeave">
-        <div v-if="show" class="bg-primary-focus overflow-hidden">
+        <div v-if="show" class="overflow-hidden bg-primary-focus">
             <div class="h-12">sss</div>
         </div>
     </transition>
 </template>
 
 <script setup>
-import ChengIcone from "@/component/Dashboard/ChengIcone.vue";
+import SubTitle from "@/component/Dashboard/SubTitle.vue";
 
 import ChevronUp from "@/component/Icons/ChevronUp.vue";
 import ChevronDown from "@/component/Icons/ChevronDown.vue";
 
 import { open, close } from "@/functions/Anime";
-
-import { ref } from "@vue/reactivity";
 
 defineProps(["quiz", "show", "index"]);
 defineEmits(["showThis"]);

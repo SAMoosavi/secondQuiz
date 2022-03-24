fd66410822cfee4620b90632e24c59111a5e7264
Moosavi <moosavi238@gmail.com>
2022-03-24 16:27:25 +0430

    edit answers table

---
+++ database/migrations/2022_03_24_114924_create_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('question_id')->constrained('questions')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('type');
+            $table->text('answer')->nullable();
+            $table->double('score', 8, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};

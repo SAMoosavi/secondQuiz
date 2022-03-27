54c5131de14524df341c4660858afe4e20cba8ee
Moosavi <moosavi238@gmail.com>
2022-03-27 21:35:42 +0430

    create add and remove function and set state property

--- resources/js/store/CreatQuiz.js
+++ resources/js/store/CreatQuiz.js
@@ -3,13 +3,27 @@ import {defineStore} from 'pinia'
 export const useCreatQuiz = defineStore('CreatQuiz', {
     state: () => {
         return {
-            //
+            content: 0,
+            questions: {},
         }
     },
     getters: {
         //
     },
     actions: {
-        //
+        add(index, question = {}) {
+            if (this.questions[index]) {
+                this.questions[index] = question;
+            } else {
+                this.content++;
+                this.questions = {...this.questions, [index]: question};
+            }
+            localStorage.setItem(index, JSON.stringify(question))
+        },
+        remove(index) {
+            this.content--;
+            delete this.questions[index];
+            localStorage.removeItem(index)
+        },
     }
 })

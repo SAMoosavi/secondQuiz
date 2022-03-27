0e5b1c97e836b24fdd104b83d3e0158dc5289fcf
Moosavi <moosavi238@gmail.com>
2022-03-27 21:36:08 +0430

    create textarea component

---
+++ resources/js/component/Form/Textarea.vue
@@ -0,0 +1,17 @@
+<template>
+   <textarea
+       :id="id"
+       v-model="modelValue"
+       :disabled="disabled"
+       class="w-full bg-transparent border-1 focus:border border-primary textarea outline-0 focus:outline-0 focus:border-primary-focus focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+       rows="1"
+       @input="$emit('update:modelValue', $event.target.value)"
+   ></textarea>
+
+</template>
+
+<script setup>
+defineProps(["modelValue", "id", "disabled"]);
+defineEmits(["update:modelValue"]);
+</script>
+

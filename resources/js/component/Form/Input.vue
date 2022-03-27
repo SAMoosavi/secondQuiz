c4d812ebde5e2637a9686026f2b1f9e7100ee539
Moosavi <moosavi238@gmail.com>
2022-03-27 16:24:38 +0430

    edit and add_Icon_in_form_input

--- resources/js/component/Form/Input.vue
+++ resources/js/component/Form/Input.vue
@@ -1,20 +1,49 @@
 <template>
+    <!--Has Icon-->
+    <div v-if="icon" class="flex">
+        <button
+            :disabled="disabled"
+            class="btn btn-primary rounded-l-none"
+            @click="click"
+        >
+            <slot/>
+        </button>
+        <input
+            :id="id"
+            ref="input"
+            :class="classInput"
+            :disabled="disabled"
+            :value="modelValue"
+            class="rounded-r-none"
+            type="text"
+            @input="$emit('update:modelValue', $event.target.value)"
+        >
+    </div>
+    <!--Hasn't Icon-->
     <input
-        class="w-full bg-transparent border-1 focus:border border-primary input input-bordered outline-0 focus:outline-0 focus:border-primary-focus focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+        v-else
+        :id="id"
+        ref="input"
+        :class="classInput"
+        :disabled="disabled"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
     />
 </template>
 
 <script setup>
-import { ref } from "@vue/reactivity";
-
-defineProps(["modelValue"]);
-
+// Vue function
+import {ref} from "@vue/reactivity";
+// Props & Emits
+defineProps(["modelValue", "icon", "id", "disabled"]);
 defineEmits(["update:modelValue"]);
+// Class Input
+const classInput = `w-full bg-transparent border-1 focus:border border-primary input input-bordered outline-0 focus:outline-0 focus:border-primary-focus focus:ring-2 focus:ring-primary focus:ring-opacity-50`
+// Input ref
 const input = ref();
-function focus() {
-    input.value.focus();
+
+// click function
+function click() {
+    input.value.click();
 }
 </script>

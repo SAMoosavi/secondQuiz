bedcc3152ef3355cd2397976bcb0e08caf57a06c
Moosavi <moosavi238@gmail.com>
2022-03-28 18:24:55 +0430

    add component for dashboard

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -1,19 +1,17 @@
 <template>
-    <app-layout title="Dashboard">
+    <app-layout :title="props.title">
         <div
-            class="fixed inset-0 right-0 z-20 w-1/5 overflow-y-auto bg-base-200 top-16 text-base-content"
+            class="fixed inset-0 right-0 w-1/4 lg:w-1/5 overflow-y-auto bg-base-200 top-16 text-base-content"
         >
-            <quiz-index :show="showQuiz" @chengShow="showQuiz = !showQuiz" />
+            <quiz-index :show="showQuiz" :showIndex="showIndex" @chengShow="showQuiz = !showQuiz"/>
 
-            <answer-index :show="!showQuiz" @chengShow="showQuiz = !showQuiz" />
+            <answer-index :show="!showQuiz" :showIndex="showIndex" @chengShow="showQuiz = !showQuiz"/>
         </div>
         <div
-            class="fixed inset-0 left-0 z-10 w-auto overflow-y-auto bg-neutral top-16"
+            class="fixed inset-y-0 left-0 overflow-y-auto bg-base-100 top-16 w-3/4 lg:w-4/5 overflow-hidden"
         >
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-xl sm:rounded-lg"
-                ></div>
+            <div class=" sm:p-6 lg:p-8 pb-0">
+                <component :is="components[props.component]" :props="props.myProps"/>
             </div>
         </div>
     </app-layout>
@@ -25,8 +23,37 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 // Call Menus
 import QuizIndex from "@/Pages/Dashboard/Menu/Quiz/Index.vue";
 import AnswerIndex from "@/Pages/Dashboard/Menu/Answer/Index.vue";
+// components in main
+import CreateQuiz from "@/Pages/Dashboard/Component/CreateQuiz/Index.vue"
 // vue functions
-import { ref } from "@vue/reactivity";
+import {ref} from "@vue/reactivity";
+// Props
+const props = defineProps({
+    title: {
+        type: String,
+        default: "ساخت آزمون جدید",
+    },
+    showQuiz: {
+        type: Boolean,
+        default: true,
+    },
+    showIndex: {
+        type: Number,
+        default: -1,
+    },
+    component: {
+        type: String,
+        default: 'create-quiz',
+    },
+    myProps: {
+        type: Object,
+        default: {},
+    }
+})
 // show Quiz Or Answer
-const showQuiz = ref(true);
+const showQuiz = ref(props.showQuiz);
+
+const components = {
+    'create-quiz': CreateQuiz,
+}
 </script>

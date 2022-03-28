f942c0f1c81da4de9a224e379b64b99886a16aec
Moosavi <moosavi238@gmail.com>
2022-03-28 18:20:24 +0430

    create Radio.vue component

---
+++ resources/js/component/Form/Radio.vue
@@ -0,0 +1,15 @@
+<template>
+    <input
+        :name="name"
+        :value="value"
+        class="ring-0 focus:ring-0 focus:ring-offset-0 focus:outline-0 bg-transparent radio-sm text-primary border-primary"
+        type="radio"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<script setup>
+defineProps(["value", "name"]);
+defineEmits(['update:modelValue'])
+</script>
+

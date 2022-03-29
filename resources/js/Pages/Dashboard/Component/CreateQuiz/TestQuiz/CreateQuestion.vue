c90821622ded4117fad6fc07dbfb190c47f90e12
Moosavi <moosavi238@gmail.com>
2022-03-29 21:07:39 +0430

    create Test/CreateQuestion.vue

---
+++ resources/js/Pages/Dashboard/Component/CreateQuiz/TestQuiz/CreateQuestion.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="grid grid-cols-1">
+        <div class="form-control">
+            <my-label :required="true" for="question" value="سؤال"/>
+            <my-textarea id="question" v-model.lazy="question.question"/>
+        </div>
+    </div>
+
+    <test
+        :index="props.index"
+        @answer="getAnswer"
+        @option="getOption"
+    />
+</template>
+
+<script setup>
+// Form component
+import MyLabel from "@/component/Form/Label.vue";
+import MyTextarea from "@/component/Form/Textarea.vue";
+//Pinia
+import {useCreatQuiz} from "@/store/CreatQuiz";
+// types
+import Test from "../Types/Test.vue";
+// Vue function
+import {onMounted, watch} from "vue";
+import {reactive} from "@vue/reactivity";
+// Props
+const props = defineProps(["index"]);
+/**************** Pinia ****************/
+const CreateQuiz = useCreatQuiz();
+// create pinia actions
+const {add} = CreateQuiz;
+/**************** Properties ****************/
+// question object
+const question = reactive({
+    question: null,
+    score: null,
+    answer: null,
+    type: 'test',
+    option: {},
+});
+// onMounted
+onMounted(() => add(props.index, question))
+
+/**************** Functions ****************/
+// getOption
+function getOption(val) {
+    question.option = val;
+    add(props.index, question);
+}
+
+// getAnswer
+function getAnswer(val) {
+    question.answer = val;
+    add(props.index, question);
+}
+
+// watch question
+watch(
+    () => question.question,
+    () => add(props.index, question)
+);
+// watch score
+watch(
+    () => question.score,
+    () => add(props.index, question)
+);
+</script>
+

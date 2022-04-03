5996fce43bd95bc6af86cff0c6efcdaa0abdfe08
Moosavi <moosavi238@gmail.com>
2022-04-03 16:07:32 +0430

    create store in QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
+use Date\Jalali;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+use JetBrains\PhpStorm\NoReturn;
 
 class QuizController extends Controller
 {
-    //
+
+    #[NoReturn] public function store(Request $request)
+    {
+        $request->validate([
+            'name' => "required|string",
+            'start' => 'nullable|string',
+            'end' => 'nullable|string',
+            'time' => 'required|string',
+            'score' => 'required|numeric',
+            'scoreN' => 'nullable|numeric',
+            'questions.*.question' => 'required|string',
+            'questions.*.type' => 'required',
+            'questions.*.option' => 'nullable|array',
+            'questions.*.answer' => 'nullable|string',
+            'questions.*.score' => 'nullable|numeric',
+        ]);
+        $user = auth();
+        $quiz = Quiz::create([
+            'name' => $request->name,
+            'user_id' => $user->id(),
+            'start' => !!$request->start ? (new Jalali($request->start))->toGregorian()->format('Y-m-d H:i:s') : null,
+            'end' => !!$request->end ? (new Jalali($request->end))->toGregorian()->format('Y-m-d H:i:s') : null,
+            'time' => $request->time,
+            'score' => $request->score,
+            'uuid' => (string)Str::uuid(),
+        ]);
+
+        foreach ($request->questions as $question) {
+            QuestionController::store($question, $quiz->id);
+        }
+
+        return Redirect::route('show.quiz', ['quiz' => $quiz->uuid]);
+    }
 }

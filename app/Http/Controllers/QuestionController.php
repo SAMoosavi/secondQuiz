427f73371b6d69076c169f205e09addb41ba382a
Moosavi <moosavi238@gmail.com>
2022-04-03 16:07:10 +0430

    create store in QuestionController.php

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
-    //
+    static public function store($question,$quizId){
+        Question::create([
+            'question' => $question['question'],
+            'type' => $question['type'],
+            'option' =>  json_encode($question['option']),
+            'answer' => $question['answer'],
+            'score' => (float)$question['score'],
+            'quiz_id' => $quizId,
+        ]);
+    }
 }

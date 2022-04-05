52283ae595ccecf7ec8a235914c1f4e7d146ae98
Moosavi <moosavi238@gmail.com>
2022-04-05 11:53:05 +0430

    add fillable & Quiz

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'question',
+        'type',
+        'option',
+        'answer',
+        'quiz_id',
+        'score',
+    ];
+
+    public function Quiz(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }

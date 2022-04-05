7c9ce5b13d3a0340d3cd6960d6cf8fd35c431ecc
Moosavi <moosavi238@gmail.com>
2022-04-05 11:52:47 +0430

    add fillable and  Teacher  & Questions

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'start',
+        'end',
+        'time',
+        'user_id',
+        'uuid',
+        'score',
+        'scoreN',
+        'type',
+    ];
+
+    public function Teacher(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function Questions(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
 }

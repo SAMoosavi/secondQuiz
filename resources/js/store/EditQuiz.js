ccd0d73d1e2fce69130610b88759883a5d40a321
Moosavi <moosavi238@gmail.com>
2022-04-05 07:00:43 +0430

    create EditQuiz.js pinia

---
+++ resources/js/store/EditQuiz.js
@@ -0,0 +1,30 @@
+import {defineStore} from 'pinia'
+
+export const useEditQuiz = defineStore('EditQuiz', {
+    state: () => {
+        return {
+            content: 0, scoreQuiz: 0, questions: {}, delete: [],
+        }
+    }, getters: {
+        //
+    }, actions: {
+        add(index, question = {}, score = 0) {
+            if (this.questions[index]) {
+                this.scoreQuiz += +score;
+                this.questions[index] = question;
+            } else {
+                this.content++;
+                this.scoreQuiz += question.score;
+                this.questions = {...this.questions, [index]: question};
+            }
+        }, remove(index) {
+            this.content--;
+            this.scoreQuiz -= +this.questions[index].score;
+            delete this.questions[index];
+            this.delete.push(index);
+        }, clean() {
+            this.content = 0;
+            this.questions = {};
+        }
+    }
+})

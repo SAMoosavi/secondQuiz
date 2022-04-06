7877d789ec754513d913c93cfe5b6979fa140ad6
Moosavi <moosavi238@gmail.com>
2022-04-06 15:56:40 +0430

    add Toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,29 @@
+import {createApp, h} from "vue";
+import {createInertiaApp} from "@inertiajs/inertia-vue3";
+import {InertiaProgress} from "@inertiajs/progress";
+import {createPinia} from 'pinia'
+import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import {color} from "@/Consts/MyConsts";
+
 require("./bootstrap");
 
-import { createApp, h } from "vue";
-import { createInertiaApp } from "@inertiajs/inertia-vue3";
-import { InertiaProgress } from "@inertiajs/progress";
-import { createPinia } from 'pinia'
-import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+    setup({el, app, props, plugin}) {
+        return createApp({render: () => h(app, props)})
             .use(plugin)
             .use(createPinia())
             .use(Vue3PersianDatetimePicker)
-            .mixin({ methods: { route } })
+            .use(Toast)
+            .mixin({methods: {route}})
             .mount(el);
     },
 });
 
-InertiaProgress.init({ color: "#4B5563" });
+InertiaProgress.init({color: color});

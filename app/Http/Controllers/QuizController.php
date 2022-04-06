be449d49124ff451fe9b8b15451518d288fa4b6b
Moosavi <moosavi238@gmail.com>
2022-04-06 15:51:34 +0430

    config edit function

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,7 +7,6 @@ use Date\Jalali;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
-use JetBrains\PhpStorm\NoReturn;
 
 class QuizController extends Controller
 {
@@ -48,8 +47,43 @@ class QuizController extends Controller
         return Redirect::route('teacher.information.quiz', ['quiz' => $quiz->uuid]);
     }
 
-    #[NoReturn] public function edit(Request $request)
+    public function edit(Quiz $quiz, Request $request)
     {
-        dd($request);
+        $request->validate([
+            'id' => 'required|integer',
+            'name' => "required|string",
+            'start' => 'nullable|string',
+            'end' => 'nullable|string',
+            'time' => 'required|string',
+            'score' => 'required|numeric',
+            'scoreN' => 'nullable|numeric',
+            'type' => 'required|string',
+            'questions.*.question' => 'required|string',
+            'questions.*.type' => 'required',
+            'questions.*.option' => 'nullable',
+            'questions.*.answer' => 'nullable|string',
+            'questions.*.score' => 'nullable|numeric',
+            'deleted' => 'nullable|array'
+        ]);
+        $quiz->update([
+            'name' => $request->name,
+            'start' => !!$request->start ? (new Jalali($request->start))->toGregorian()->format('Y-m-d H:i:s') : null,
+            'end' => !!$request->end ? (new Jalali($request->end))->toGregorian()->format('Y-m-d H:i:s') : null,
+            'time' => $request->time,
+            'score' => $request->score,
+            'scoreN' => $request->scoreN,
+            'type' => $request->type,
+        ]);
+        foreach ($request->questions as $key => $question) {
+            if (is_int($key)) {
+                QuestionController::edit($question, $key);
+            } else {
+                QuestionController::store($question, $request->id);
+            }
+        }
+        foreach ($request->deleted as $deleted) {
+            QuestionController::deleted($deleted);
+        }
+        return Redirect::route('teacher.information.quiz', ['quiz' => $quiz->uuid]);
     }
 }

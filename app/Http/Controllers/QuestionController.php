33f3d27579ec1cb90ac41b83dcdff55a4504ee98
Moosavi <moosavi238@gmail.com>
2022-04-06 15:52:39 +0430

    config and add edit and deleted function

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,19 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
-    static public function store($question,$quizId){
+    static public function store($question, $quizId)
+    {
         Question::create([
             'question' => $question['question'],
             'type' => $question['type'],
-            'option' =>  json_encode($question['option']),
+            'option' => json_encode($question['option']),
             'answer' => $question['answer'],
             'score' => (float)$question['score'],
             'quiz_id' => $quizId,
         ]);
+    }
+
+    static public function edit($question, $key)
+    {
+        Question::find($key)->update([
+            'question' => $question['question'],
+            'type' => $question['type'],
+            'option' => $question['option'],
+            'answer' => $question['answer'],
+            'score' => (float)$question['score'],
+        ]);
+
+    }
+
+    static public function deleted($deleted)
+    {
+        Question::find($deleted)->delete();
     }
 }

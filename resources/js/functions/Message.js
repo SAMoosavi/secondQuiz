8cc4d7def2149bde63d9c9f596c62ea0684e4a26
Moosavi <moosavi238@gmail.com>
2022-04-07 10:35:58 +0430

    add toast

--- resources/js/functions/Message.js
+++ resources/js/functions/Message.js
@@ -1,12 +1,72 @@
-export function successMessage(text){
-    console.log(text);
+import {useToast} from "vue-toastification";
+
+
+const toast = useToast();
+
+export function successMessage(text) {
+    toast.success(text, {
+        position: "bottom-right",
+        timeout: 5000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+    });
 }
-export function errorMessage(text){
-    console.log(text);
+
+export function errorMessage(text) {
+    toast.error(text, {
+        position: "bottom-right",
+        timeout: 5000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+    });
 }
-export function infoMessage(text){
-    console.log(text);
+
+export function infoMessage(text) {
+    toast.info(text, {
+        position: "bottom-right",
+        timeout: 5000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+    });
 }
-export function warningMessage(text){
-    console.log(text);
+
+export function warningMessage(text) {
+    toast.warning(text, {
+        position: "bottom-right",
+        timeout: 5000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+    });
 }

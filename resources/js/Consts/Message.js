8c8993aa78d455dfd1a3f7f55368adbdf2936101
Moosavi <moosavi238@gmail.com>
2022-04-07 10:42:39 +0430

    create Message.js and property.js and set value

---
+++ resources/js/Consts/Message.js
@@ -0,0 +1,6 @@
+export const requiredMessage = 'تمام فیلد های ستاره دار را پر کنید';
+export const emailMessage = 'ایمیل وارد شده صحیح نمی باشد';
+export const confirmPasswordMessage = 'تأیید رمز عبور با رمز عبور باید یکسان باشد';
+export const requiredAnsMessage = 'پاسخ تمام سؤالات تستی را ثبت کنید';
+export const scoreMessage = 'نمره باید به صورت عدیدی بزرگ تر از صفر باشد';
+export const scoreNMessage = 'نمره منفی باید به صورت عدیدی مثبت و بدون اعشار باشد';

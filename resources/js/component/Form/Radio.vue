1d9ae84792cf26797b8262c53165ff2b471d027b
Moosavi <moosavi238@gmail.com>
2022-04-08 16:00:01 +0430

    add txt

--- resources/js/component/Form/Radio.vue
+++ resources/js/component/Form/Radio.vue
@@ -1,16 +1,21 @@
 <template>
-    <input
-        :name="name"
-        :value="value"
-        class="ring-0 focus:ring-0 focus:ring-offset-0 focus:outline-0 bg-transparent radio-sm text-primary border-primary"
-        type="radio"
-        @input="$emit('update:modelValue', $event.target.value)"
-        :required="required"
-    />
+    <my-label class="justify-start gap-2">
+        <input
+            :name="name"
+            :required="required"
+            :value="value"
+            class="ring-0 focus:ring-0 focus:ring-offset-0 focus:outline-0 bg-transparent radio-sm text-primary border-primary"
+            type="radio"
+            @input="$emit('update:modelValue', $event.target.value)"
+        />
+        <span class="label-text">{{ txt }}</span>
+    </my-label>
 </template>
 
 <script setup>
-defineProps(["value", "name","required"]);
+import MyLabel from "@/component/Form/Label.vue";
+
+defineProps(["value", "name", "required", 'txt']);
 defineEmits(['update:modelValue'])
 </script>
 

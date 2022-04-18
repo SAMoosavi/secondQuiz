da93a4a0574693f6617150950c5872505a927dc1
Moosavi <moosavi238@gmail.com>
2022-04-18 14:04:01 +0430

    add comment for phpStorm

--- app/Models/StudentQuiz.php
+++ app/Models/StudentQuiz.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static where(string $string, string $string1, int|string|null $userId)
+ * @method static create(array $array)
+ * @property mixed $created_at
+ */
 class StudentQuiz extends Model
 {
     use HasFactory;

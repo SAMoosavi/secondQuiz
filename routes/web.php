286c823072f3938d1c7700956a86d5731fa67896
Moosavi <moosavi238@gmail.com>
2022-04-20 10:50:30 +0430

    creat get.students route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\StudentQuizController;
+use App\Models\Quiz;
+use App\Models\StudentQuiz;
+use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,11 +22,13 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard/Dashboard');
 })->name('dashboard');
 Route::get('/create-quiz', function () {
-    return Inertia::render('Dashboard/Dashboard', ['showQuiz' => true, 'showIndex' => -1, 'component' => 'create-quiz']
+    return Inertia::render(
+        'Dashboard/Dashboard',
+        ['showQuiz' => true, 'showIndex' => -1, 'component' => 'create-quiz']
     );
 })->name('create.quiz');
 Route::post('/create-quiz', [QuizController::class, 'store'])->name('store.quiz');
-Route::get('/teacher/edit/{quiz:uuid}', function (\App\Models\Quiz $quiz) {
+Route::get('/teacher/edit/{quiz:uuid}', function (Quiz $quiz) {
     $quiz->start = !!$quiz->start ? (new \Date\Date($quiz->start))->toJalali()->format('Y-m-d H:i:s') : null;
     $quiz->end = !!$quiz->end ? (new \Date\Date($quiz->end))->toJalali()->format('Y-m-d H:i:s') : null;
 
@@ -38,8 +43,8 @@ Route::get('/teacher/edit/{quiz:uuid}', function (\App\Models\Quiz $quiz) {
     ]);
 })->name('teacher.edit.quiz');
 Route::put('/teacher/edit/{quiz:uuid}', [QuizController::class, 'edit'])->name('edit.quiz');
-Route::get('/teacher/information/{quiz:uuid}', function (\App\Models\Quiz $quiz) {
-//    dd($quiz, $quiz->Teacher, $quiz->Questions);
+Route::get('/teacher/information/{quiz:uuid}', function (Quiz $quiz) {
+    //    dd($quiz, $quiz->Teacher, $quiz->Questions);
     return Inertia::render('Dashboard/Dashboard', [
         'showQuiz' => true,
         'showIndex' => $quiz->uuid,
@@ -51,7 +56,15 @@ Route::post('/student/answered/descriptive', [AnswerController::class, 'storeDes
 Route::post('/student/answered/test', [AnswerController::class, 'storeTest'])->name('student.answered.test');
 /***************************API**************************/
 Route::get('/teacher/quiz', function () {
-    $quizzes = \App\Models\User::find(auth()->id())->TeacherQuizzes()->select(['name', 'uuid'])->latest()->get();
+    $quizzes = User::find(auth()->id())->TeacherQuizzes()->select(['name', 'uuid'])->latest()->get();
 
     return response(['quizzes' => $quizzes], 200);
 })->name('get.teacher.quizzes');
+
+Route::get('students/{quiz:uuid}', function (Quiz $quiz) {
+    $students =  StudentQuiz::where('quiz_id', '=', $quiz->id)->get()->map(function ($item) {
+        return $item->Student;
+    });
+
+    return response(['students' => $students], 200);
+})->name('get.students');
